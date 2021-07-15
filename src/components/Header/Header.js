0ccd47a425fcb9eeb7af3667d43d6bae224eaa84
Farhad Amrah <emferhad17631@sabah.edu.az>
2021-07-15 12:23:19 +0400

    version1

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Header.scss';
 
 const Header = () => {
   return (
     <>
       <header className='main-header'>
-        <div className='main-header__logo'>LOGO</div>
+        <div className='main-header__logo'>
+          <Link to='/'>LOGO</Link>
+        </div>
         <nav className='main-header__nav-menu'>
           <ul className='main-header__nav-menu--list'>
-            <li className='main-header__nav-menu--list__item'>Əsas Səhifə</li>
+            <li className='main-header__nav-menu--list__item'>
+              <Link to='/'>Əsas Səhifə </Link>
+            </li>
             <li className='main-header__nav-menu--list__item'>Haqqımızda</li>
           </ul>
         </nav>
